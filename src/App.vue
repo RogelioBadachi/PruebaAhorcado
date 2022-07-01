44915292a33fea10da49cface84813f63fbf73cf
RogelioBadachi <royer2302@gmail.com>
2022-07-01 01:08:11 -0700

    Modifique nombre del componente

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--<img alt="Vue logo" src="./assets/logo.png">-->
+  <AhorcadoView msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AhorcadoView from './components/AhorcadoView.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AhorcadoView
   }
 }
 </script>
---
+++ src/components/AhorcadoView.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="home">
+    <div class="cuadroMono">
+      <div class="monito">
+        <h3 class="holi" ref="holi">{{holi}}</h3>
+      </div>
+    </div>
+    <div class="letras">
+
+    </div>
+    <div class="escritura">
+      <button class="ingresarLetras" @click="formatting">Ingresar</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AhorcadoView',
+  props: {
+    msg: String
+  },
+
+  methods:{
+    formatting: function(){
+      this.holi = "holi";
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.cuadroMono {
+  height: 300px;
+  width: 200px;
+  border: solid 10px black;
+}
+</style>
